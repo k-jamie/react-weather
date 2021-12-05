581dfd77ca8ffd6e096284e22b359040893e177b
k-jamie <79115673+k-jamie@users.noreply.github.com>
2021-12-05 18:26:44 +0000

    integrated axios and fetched temp data from api

--- src/Weather.js
+++ src/Weather.js
@@ -1,64 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  let weatherData = {
-    city: "London",
-    temperature: 13,
-    wind: 7,
-    humidity: 72,
-    date: "Sunday 15:41",
-    description: "Few clouds",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-  };
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  function handleResponse(response) {
+    console.log(response.data)
+    setTemperature(response.data.main.temp);
+    setReady(true);
+  }
 
-  return (
-    <div className="App">
-      <div className="Weather">
-        <form>
+  if (ready) {
+    return (
+      <div className="App">
+        <div className="Weather">
+          <form>
+            <div className="row">
+              <div className="col-8">
+                <input
+                  type="search"
+                  placeholder="Type a city..."
+                  className="form-control"
+                  autoComplete="off"
+                />
+              </div>
+              <div className="col-4">
+                <input type="submit" value="Search" className="btn w-100" />
+              </div>
+            </div>
+          </form>
           <div className="row">
-            <div className="col-8">
-              <input
-                type="search"
-                placeholder="Type a city..."
-                className="form-control"
-                autoComplete="off"
-              />
+            <div className="col-6">
+              <img src="" className="main-weather-icon" />
+              <h2>Few clouds</h2>
             </div>
-            <div className="col-4">
-              <input type="submit" value="Search" className="btn w-100" />
-            </div>
-          </div>
-        </form>
-        <div className="row">
-          <div className="col-6">
-            <img src={weatherData.imgUrl} className="main-weather-icon" alt="placeholder weather icon" />
-            <h2>{weatherData.description}</h2>
-          </div>
 
-          <div className="col-6">
-            <div className="overview">
-              <p className="date-time">{weatherData.date}</p>
-              <p className="location">{weatherData.city}</p>
-              <h1>{weatherData.temperature}ºC</h1>
-              <p>
-                <a href="/" className="active">
-                  celsius
+            <div className="col-6">
+              <div className="overview">
+                <p className="date-time">Sunday</p>
+                <p className="city">London</p>
+                <p className="unit">{Math.round(temperature)}<span className="degree">º</span></p>
+                <p>
+                  <a href="/" className="active">
+                    celsius
                 </a>
                 |<a href="/"> fahrenheit</a>
-              </p>
-              <ul>
-                <li className="wind">{weatherData.wind}mp/h</li>
-                <li>{weatherData.humidity}%</li>
-              </ul>
+                </p>
+                <ul>
+                  <li className="wind">7mp/h</li>
+                  <li>72%</li>
+                </ul>
+              </div>
             </div>
           </div>
+          <hr />
         </div>
-        <hr />
-    
       </div>
-      <a href="https://github.com/k-jamie/react-weather" className="source-link" target="_blank" rel="noreferrer">Open-source code</a>, by Karen Jamieson
-    </div>
-  );
+    );
+  } else {
+
+    let apiKey = "bef9f91b8c0ad49d1982384e026f7513";
+    let city = "London"
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+ 
+    return "Loading..."
+  }
+
+ 
 }
