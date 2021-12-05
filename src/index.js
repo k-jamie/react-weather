c6158c590ca1e662266b4b31f321db89486d7223
k-jamie <79115673+k-jamie@users.noreply.github.com>
2021-12-05 21:44:26 +0000

    updated css to include bckground image and footer text

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import Weather from "./Weather";
 
 import "./App.css";
 
+
 function App() {
   return (
     <div className="App">
       <Weather defaultCity="London" />
+      <p className="small">👩‍💻 This project was coded by <span class="name">Karen Jamieson</span> and is open-sourced on <a href="https://github.com/k-jamie/vanilla-weather-app" class="project-links">GitHub</a> and hosted on <a href="https://nervous-keller-4ac341.netlify.app/" class="project-links">Netlify</a>.</p>
       
     </div>
   );
