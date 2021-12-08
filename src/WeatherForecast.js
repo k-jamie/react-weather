d13d14428575da0ca2e24dfde0c1611aab7ff753
k-jamie <79115673+k-jamie@users.noreply.github.com>
2021-12-08 23:22:12 +0000

    created new component to handle and format the forecast data

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
+import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
+
     function handleResponse(response) {
-        console.log(response.data);
+        setForecast(response.data.daily);
+        setLoaded(true);
 }
 
-    let apiKey = "669a454c793bb7415176348de919e355";
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
-    return (<div className="Forecast">
+    if (loaded) {
+        console.log(forecast);
+    return (
+     <div className="Forecast">
         <div className="row">
             <div className="col">
-                <div className="ForecastDate">Tue</div>
-           <div className="ForecastIcon"><WeatherIcon code="01d" size={50} /></div> 
-                <div className="ForecastTemperatures"><span className="ForecastTempMax">8º</span>5º</div>
+                   <WeatherForecastDay data={forecast[0]} />
             </div>
-            </div>
-    </div>
-    );
+        </div>
+     </div>
+    );  
+
+       
+    } else {
+        let apiKey = "669a454c793bb7415176348de919e355";
+        let longitude = props.coordinates.lon;
+        let latitude = props.coordinates.lat;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+        axios.get(apiUrl).then(handleResponse);
+
+        return null;
+}
+ 
     
 }
