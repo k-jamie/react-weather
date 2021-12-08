e6078dbfebc80f11f8003fd8a976c74f2a92de2b
k-jamie <79115673+k-jamie@users.noreply.github.com>
2021-12-08 22:36:47 +0000

    updated forecast component to include api call

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,19 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
+import axios from "axios";
 
-export default function WeatherForecast() {
-    return <div className="Forecast">
+export default function WeatherForecast(props) {
+    function handleResponse(response) {
+        console.log(response.data);
+}
+
+    let apiKey = "669a454c793bb7415176348de919e355";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return (<div className="Forecast">
         <div className="row">
             <div className="col">
                 <div className="ForecastDate">Tue</div>
@@ -10,6 +21,7 @@ export default function WeatherForecast() {
                 <div className="ForecastTemperatures"><span className="ForecastTempMax">8º</span>5º</div>
             </div>
             </div>
-        </div>;
+    </div>
+    );
     
 }
